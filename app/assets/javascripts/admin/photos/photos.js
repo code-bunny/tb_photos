4c69e0544bf3a2e1091a40f764055509f54764d1
Greg Woods <greg@westlakedesign.com>
2014-11-30 10:39:56 -0500

    css class name refactor

--- app/assets/javascripts/admin/photos/photos.js
+++ app/assets/javascripts/admin/photos/photos.js
@@ -5,23 +5,23 @@ var html5upload = false;
 var self = spud.admin.photos = {
   init: function(){
     // event handlers
-    $('.spud_admin_photo_ui_thumbs_sortable').sortable({
-      connectWith:'.spud_admin_photo_ui_thumbs_sortable'
+    $('.admin-photo-ui-thumbs-sortable').sortable({
+      connectWith:'.admin-photo-ui-thumbs-sortable'
     });
-    $('body').on('submit', '#spud_admin_photo_album_form', submittedPhotoAlbumForm);
-    $('body').on('submit', '#spud_admin_photo_gallery_form', submittedPhotoGalleryForm);
-    $('body').on('submit', '#spud_admin_photo_form', submittedPhotoForm);
-    $('body').on('click', '.spud_admin_photos_btn_remove', clickedPhotoRemoveFromAlbum);
-    $('body').on('click', '.spud_admin_photo_ui_thumbs_selectable .spud_admin_photo_ui_thumb', selectedPhotoUiThumb);
-    $('body').on('click', '#spud_admin_photo_album_action_library', clickedPhotoLibrary);
-    $('body').on('click', '#spud_admin_photo_album_action_upload, .spud_admin_photo .spud_admin_photos_btn_edit', clickedPhotoAddOrEdit);
-    $('body').on('click', '.spud_admin_photo_library_add_selected', addSelectedPhotosFromLibrary);
-    $('body').on('click', '.spud_admin_photo_library_delete_selected', deleteSelectedPhotosFromLibrary);
+    $('body').on('submit', '#admin-photo-album-form', submittedPhotoAlbumForm);
+    $('body').on('submit', '#admin-photo-gallery-form', submittedPhotoGalleryForm);
+    $('body').on('submit', '#admin-photo-form', submittedPhotoForm);
+    $('body').on('click', '.admin-photos-btn-remove', clickedPhotoRemoveFromAlbum);
+    $('body').on('click', '.admin-photo-ui-thumbs-selectable .admin-photo-ui-thumb', selectedPhotoUiThumb);
+    $('body').on('click', '#admin-photo-album-action-library', clickedPhotoLibrary);
+    $('body').on('click', '#admin-photo-album-action-upload, .admin-photo .admin-photos-btn-edit', clickedPhotoAddOrEdit);
+    $('body').on('click', '.admin-photo-library-add-selected', addSelectedPhotosFromLibrary);
+    $('body').on('click', '.admin-photo-library-delete-selected', deleteSelectedPhotosFromLibrary);
 
     // html5 drag and drop file
-    if(typeof(FormData) != 'undefined' && typeof(XMLHttpRequest) != 'undefined' && (droparea = document.getElementById('spud_admin_photo_upload_queue'))){
+    if(typeof(FormData) != 'undefined' && typeof(XMLHttpRequest) != 'undefined' && (droparea = document.getElementById('admin-photo-upload-queue'))){
       html5upload = true;
-      $('#spud_admin_photo_upload_queue').show();
+      $('#admin-photo-upload-queue').show();
       droparea.addEventListener('dragenter', stopDndPropagation, false);
       droparea.addEventListener('dragexit', stopDndPropagation, false);
       droparea.addEventListener('dragover', stopDndPropagation, false);
@@ -48,16 +48,16 @@ var self = spud.admin.photos = {
   /* Handle file uploads passed via iframe (legacy support)
   * -------------------------------------------------------- */
   photoLegacyUploadErrors: function(html){
-    $('#spud_admin_photo_form').replaceWith(html);
+    $('#admin-photo-form').replaceWith(html);
   },
 
   photoLegacyUploadComplete: function(id, html){
-    var element = $('#spud_admin_photo_' + id);
+    var element = $('#admin-photo-' + id);
     if(element.length > 0){
       element.replaceWith(html);
     }
     else{
-      var target = $('#spud_admin_photos_selected, #spud_admin_photos');
+      var target = $('#admin-photos-selected, #admin-photos');
       target.prepend(html);
     }
     spud.admin.modal.hide();
@@ -69,11 +69,11 @@ var submittedPhotoAlbumForm = function(e){
 };
 
 var submittedPhotoGalleryForm = function(e){
-  $('#spud_admin_photo_albums_available .spud_admin_photo_ui_thumb').remove();
+  $('#admin-photo-albums-available .admin-photo-ui-thumb').remove();
 };
 
 var clickedPhotoRemoveFromAlbum = function(e){
-  $(this).parents('.spud_admin_photo_ui_thumb').fadeOut(200, function(){
+  $(this).parents('.admin-photo-ui-thumb').fadeOut(200, function(){
     $(this).remove();
   });
   return false;
@@ -101,30 +101,30 @@ var validatePhoto = function(file) {
 
 var selectedPhotoUiThumb = function(e){
   var thumb = $(this);
-  if(thumb.hasClass('spud_admin_photo_ui_thumb_selected')){
-    $(this).removeClass('spud_admin_photo_ui_thumb_selected');
+  if(thumb.hasClass('admin-photo-ui-thumb-selected')){
+    $(this).removeClass('admin-photo-ui-thumb-selected');
   }
   else{
-    $(this).addClass('spud_admin_photo_ui_thumb_selected');
+    $(this).addClass('admin-photo-ui-thumb-selected');
   }
 };
 
 var markPhotoAsDeleted = function(photo_id){
-  var photo = $('#spud_admin_photo_' + photo_id);
+  var photo = $('#admin-photo-' + photo_id);
   photo.fadeOut(200, function(){
     photo.remove();
   });
 };
 
 var markPhotoAlbumAsDeleted = function(photo_album_id){
-  var photo_album = $('#spud_admin_photo_album_' + photo_album_id);
+  var photo_album = $('#admin-photo-album-' + photo_album_id);
   photo_album.fadeOut(200, function(){
     photo_album.remove();
   });
 };
 
 var markPhotoGalleryAsDeleted = function(photo_gallery_id){
-  var photo_gallery = $('#spud_admin_photo_gallery_' + photo_gallery_id);
+  var photo_gallery = $('#admin-photo-gallery-' + photo_gallery_id);
   photo_gallery.fadeOut(200, function(){
     photo_gallery.remove();
   });
@@ -149,12 +149,12 @@ var generateFileUploadErrors = function(errors, opts) {
     return false;
   }
 
-  $(".spud_admin_form_error_list").remove();
-  $("#spud_admin_photo_form").find("[for=spud_photo_photo]").css("color","black");
-  $("#spud_admin_photo_form").before('<div class="spud_admin_form_error_list"><ul><h4>' + errorMsg +'</h4></ul></div>');
-  $("#spud_admin_photo_form").find("[for=spud_photo_photo]").css("color","red");
+  $(".form-errors").remove();
+  $("#admin-photo-form").find("[for=spud_photo_photo]").css("color","black");
+  $("#admin-photo-form").before('<div class="form-errors"><h4>' + errorMsg +'</h4><ul></ul></div>');
+  $("#admin-photo-form").find("[for=spud_photo_photo]").css("color","red");
   for(var i = 0; i < errors.length; i++) {
-    $(".spud_admin_form_error_list > ul").append("<li>"+ errors[i] + "</li>");
+    $(".form-errors > ul").append("<li>"+ errors[i] + "</li>");
   }
   return false;
 };
@@ -205,10 +205,10 @@ var submittedPhotoForm = function(e){
 
 var progressBarForUpload = function(fileName){
   var html = [
-    '<div class="spud_admin_photo_progress">',
+    '<div class="admin-photo-progress">',
       '<h6>',
-        '<span class="spud_admin_photo_progress_filename">'+fileName+'</span>: ',
-        '<span class="spud_admin_photo_progress_status">Uploading</span>',
+        '<span class="admin-photo-progress_filename">'+fileName+'</span>: ',
+        '<span class="admin-photo-progress_status">Uploading</span>',
       '</h6>',
       '<div class="progress">',
         '<div class="progress-bar progress-bar-striped active" style="width: 0;"></div>',
@@ -222,7 +222,7 @@ var onPhotoUploadProgress = function(e, progressBar){
   var percent = Math.round(e.loaded * 100 / e.total);
   progressBar.find('.progress-bar').css({width: percent + '%'});
   if(percent == 100){
-    progressBar.find('.spud_admin_photo_progress_status').text('Processing');
+    progressBar.find('.admin-photo-progress_status').text('Processing');
   }
 };
 
@@ -230,14 +230,14 @@ var onPhotoUploadComplete = function(e, progressBar){
   // success
   var photo = $.parseJSON(e.target.response);
   if(e.target.status == 200){
-    progressBar.find('.spud_admin_photo_progress_status').text('Done!');
+    progressBar.find('.admin-photo-progress_status').text('Done!');
     progressBar.find('.progress-bar').removeClass('progress-bar-striped active').addClass('progress-bar-success');
-    var element = $('#spud_admin_photo_' + photo.id);
+    var element = $('#admin-photo-' + photo.id);
     if(element.length > 0){
       element.replaceWith(photo.html);
     }
     else{
-      var target = $('#spud_admin_photos_selected, #spud_admin_photos');
+      var target = $('#admin-photos-selected, #admin-photos');
       target.prepend(photo.html).fadeIn(200);
     }
     spud.admin.modal.hide();
@@ -249,7 +249,7 @@ var onPhotoUploadComplete = function(e, progressBar){
 };
 
 var onPhotoUploadCancel = function(e, progressBar){
-  progressBar.find('.spud_admin_photo_progress_status').text('Done!');
+  progressBar.find('.admin-photo-progress_status').text('Done!');
   progressBar.find('.progress').addClass('progress-danger');
 };
 
@@ -294,7 +294,7 @@ var clickedPhotoLibrary = function(e){
 
 var photoLibraryLoaded = function(html){
   var dialog = $('#modal_window');
-  $('#spud_admin_photos_selected .spud_admin_photo_ui_thumb').each(function(){
+  $('#admin-photos-selected .admin-photo-ui-thumb').each(function(){
     var id = $(this).attr('id');
     var dupe = dialog.find('#'+id);
     if(dupe){
@@ -305,23 +305,23 @@ var photoLibraryLoaded = function(html){
     title: 'My Photo Library',
     html: html,
     buttons:{
-      'spud_admin_photo_library_add_selected btn-primary': 'Add Selected',
-      'spud_admin_photo_library_delete_selected btn-danger': 'Delete Selected'
+      'admin-photo-library-add-selected btn-primary': 'Add Selected',
+      'admin-photo-library-delete-selected btn-danger': 'Delete Selected'
     }
   });
 };
 
 var addSelectedPhotosFromLibrary = function(e){
-  $('#spud_admin_photo_library .spud_admin_photo_ui_thumb_selected')
-    .removeClass('spud_admin_photo_ui_thumb_selected')
-    .prependTo('#spud_admin_photos_selected')
+  $('#admin-photo-library .admin-photo-ui-thumb-selected')
+    .removeClass('admin-photo-ui-thumb-selected')
+    .prependTo('#admin-photos-selected')
     .hide()
     .fadeIn(200);
   spud.admin.modal.hide();
 };
 
 var deleteSelectedPhotosFromLibrary = function(e){
-  var ids = $.map($('.spud_admin_photo_ui_thumb_selected'), function(val, i){
+  var ids = $.map($('.admin-photo-ui-thumb-selected'), function(val, i){
     return $(val).attr('rel');
   });
   $.ajax({
@@ -329,7 +329,7 @@ var deleteSelectedPhotosFromLibrary = function(e){
     url: '/admin/photos/mass_destroy',
     data: {spud_photo_ids:ids},
     success: function(data, textStatus, jqXHR){
-      $('.spud_admin_photo_ui_thumb_selected').fadeOut(200, function(){
+      $('.admin-photo-ui-thumb-selected').fadeOut(200, function(){
         $(this).remove();
       });
     },
@@ -357,7 +357,7 @@ var stopDndPropagation = function(e){
 var droppedFile = function(e){
   e.stopPropagation();
   e.preventDefault();
-  $('#spud_admin_photo_upload_queue').show();
+  $('#admin-photo-upload-queue').show();
   var files = e.dataTransfer.files;
   var i = 0;
   while(i < files.length){
@@ -379,7 +379,7 @@ var droppedFile = function(e){
 };
 
 var updateQueueCountLabel = function(){
-  $('#spud_admin_photo_upload_queue_label span').text(fileQueue.length);
+  $('#admin-photo-upload-queue_label span').text(fileQueue.length);
 };
 
 var uploadNextPhoto = function(){
@@ -399,7 +399,7 @@ var uploadNextPhoto = function(){
 
   // create a progress bar
   var progressBar = progressBarForUpload(file.name);
-  $('#spud_admin_photo_upload_queue_bars').prepend(progressBar);
+  $('#admin-photo-upload-queue_bars').prepend(progressBar);
 
   // send formdata as xhr
   var xhr = new XMLHttpRequest();
